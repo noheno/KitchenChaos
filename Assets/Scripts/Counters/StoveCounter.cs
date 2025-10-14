171d9cccbde266171c17a46de9e2f70eb22e4f85
noheno <156270740+noheno@users.noreply.github.com>
2025-10-14 18:08:38 +0800

    Update StoveCounter.cs
    烤肉途中拿走肉，放回去要重新烤（重新计时）

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -120,20 +120,20 @@ public class StoveCounter : BaseCounter,IHasProgress
                 else//该物体不是可烤熟物体
                 {
                     #region 不起作用？
-                    //if (HasBurningRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))//该物体为可烤焦物体
-                    //{
-                    //    state = State.Fried;
-                    //    burningTimer = 0;
-                    //    OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
-                    //    {
-                    //        state = this.state
-                    //    });
+                    if (HasBurningRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))//该物体为可烤焦物体
+                    {
+                        state = State.Fried;
+                        burningTimer = 0;
+                        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
+                        {
+                            state = this.state
+                        });
 
-                    //    OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs()
-                    //    {
-                    //        progressNormalized = fryingTimer / fryingRecipeSO.fryingTimerMax,
-                    //    });
-                    //}
+                        OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs()
+                        {
+                            progressNormalized = fryingTimer / fryingRecipeSO.fryingTimerMax,
+                        });
+                    }
                     #endregion
                 }
             }
